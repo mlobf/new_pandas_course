11949073adaf162f6877decdc123bcd701698b4a
mlobf <marcoslemeborbafilho@gmail.com>
2021-10-06 17:23:54 -0300

    End Section Two

--- LessonTwo/code_lesson_two/inspecting_data_frames.py
+++ LessonTwo/code_lesson_two/inspecting_data_frames.py
@@ -17,4 +17,45 @@ df = pd.read_csv("astronauts.csv")
 # print(df.info())
 
 # Descritive Statictic Info
-print(df.describe())
+# print(df.describe())
+
+# Show basic info.
+# In this case 316 rows and 19 columns
+# print(df.shape)
+
+# Show the correlation between  colunms
+# Seens to be a linear correlation.
+# print(df.corr())
+
+# Show's all counts of a column.
+# print(df["Year"].value_counts())
+
+# To get the Max value of all columns.
+print(df.max())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
