d2cba3aa152ba2a627f72c1db582ab90c20c68e1
mlobf <marcoslemeborbafilho@gmail.com>
2021-10-06 18:53:10 -0300

    Data Manipulation

---
+++ Section4-Basic_Manipulation/basic_manipulation.py
@@ -0,0 +1,22 @@
+from re import I
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("AB_NYC_2019.csv")
+
+
+# This is how we set "id" as a new index
+df = df.set_index("id")
+
+# print(df.head(3))
+
+
+# print(df.name[2539])
+
+df3 = df.groupby("room_type").mean()
+
+
+# Going back to standard index mode
+# df3 = df3.reset_index()
+
+print(df3)
---
+++ Section4-Basic_Manipulation/slicing.py
@@ -0,0 +1,8 @@
+import pandas as pb
+import numpy as np
+
+df = pb.read_csv('AB_NYC_2019.csv')
+
+
+# testes
+print(df)
